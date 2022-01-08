8bf690a0184cb4bb5e19f9f55b276f7d499c1900
Roberto Casadei <robyonrails@gmail.com>
2022-01-08 18:53:35 +0100

    Minor simplification

--- src/main/scala/examples/HelloScafi.scala
+++ src/main/scala/examples/HelloScafi.scala
@@ -27,9 +27,8 @@ object HelloScafi extends App {
   // Then, the expected result once the gradient has stabilised is: {1 -> 1, 2 -> 0, 3 -> 1, 4 -> 2, 5 -> 3}
   case class DeviceState(self: ID, exports: Map[ID,EXPORT], localSensors: Map[LSNS,Any], nbrSensors: Map[NSNS, Map[ID,Any]])
   val devices = (1 to 5).toSeq
-  val nbrRange: NSNS = NBR_RANGE
   var state: Map[ID,DeviceState] = (for(d <- devices; nbrs = Seq(d-1, d, d+1).filter(n => n > 0 && n < 6))
-    yield d -> DeviceState(d, Map.empty[ID,EXPORT], Map.empty[LSNS,Any], Map[NSNS,Map[ID,Any]](nbrRange -> (nbrs.toSet[ID].map(nbr => nbr -> Math.abs(d - nbr).toDouble)).toMap))
+    yield d -> DeviceState(d, Map.empty[ID,EXPORT], Map.empty[LSNS,Any], Map[NSNS,Map[ID,Any]](NBR_RANGE -> (nbrs.toSet[ID].map(nbr => nbr -> Math.abs(d - nbr).toDouble)).toMap))
     ).toMap
   // The following cycle performs the scheduling of rounds and simulates communication by writing on `state`
   val scheduling = devices ++ devices ++ devices ++ devices ++ devices // run 5 rounds each, in a round-robin fashion
@@ -39,7 +38,7 @@ object HelloScafi extends App {
     val export = program.round(ctx)
     state += d -> state(d).copy(exports = state(d).exports + (d -> export)) // update d's state
     // Simulate sending of messages to neighbours
-    state(d).nbrSensors(nbrRange).keySet.foreach(nbr => {
+    state(d).nbrSensors(NBR_RANGE).keySet.foreach(nbr => {
       state += nbr -> state(nbr).copy(exports = state(nbr).exports + (d -> export))
     })
     println(s"\tEXPORT: ${export}\n\tOUTPUT: ${export.root()}\n--------------")
