a67d6ffaefd3950257bbd895a14d886060a5b7a7
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2022-06-22 12:29:53 +0000

    fix: update scala version for scafi core

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 
 dependencies {
     implementation("org.scala-lang:scala-library:2.13.8")
-    implementation("it.unibo.scafi:scafi-core_2.12:1.1.5")
+    implementation("it.unibo.scafi:scafi-core_2.13:1.1.5")
 }
 
 application {
